d6263974eac4a70e05350d239ac47f0af0c3f40b
programmer-mahadi-hasan <mahadihasan.original@gmail.com>
2025-01-06 18:10:03 +0600

    footer added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Pages/Shared/Navbar';
+import Footer from '../Pages/Shared/Footer';
 
 const MainLayout = () => {
     return (
         <div className='w-11/12 mx-auto'>
             <Navbar />
             <Outlet></Outlet>
+            <Footer />
         </div>
     );
 };
